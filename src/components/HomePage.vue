47e0b3af677244b5f376c9d6c82d45b1f5d07056
gowthaman.b <gowthaman.b@m2pfintech.com>
2023-12-05 13:57:59 +0530

    add details of APIs

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,5 +1,6 @@
 <script setup>
 
+import constants from "../constants.js";
 </script>
 
 <template>
@@ -12,6 +13,25 @@
       <router-link to="/users">View List Of Users</router-link>
     </li>
   </ul>
+  <h2 class="mt-4">How this application is built</h2>
+  <h5>these are the list of API's</h5>
+  <div class="list-group">
+    <div class="list-group-item">Fetch All Blogs <br />
+      <em>{{constants.Blog.all}}</em>
+    </div>
+    <div class="list-group-item">Fetch All Users <br />
+      <em>{{constants.User.all}}</em>
+    </div>
+    <div class="list-group-item">Fetch Comments of a Blog , for a given id<br />
+      <em>{{constants.Blog.comments("id")}}</em>
+    </div>
+    <div class="list-group-item">Fetch Details  of a user , for a given id<br />
+      <em>{{constants.User.id("id")}}</em>
+    </div>
+    <div class="list-group-item">Fetch Comments Made by a user , for a given id<br />
+      <em>{{constants.User.comments("id")}}</em>
+    </div>
+  </div>
 </template>
 
 <style scoped>
